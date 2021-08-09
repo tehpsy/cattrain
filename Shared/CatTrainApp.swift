ad9d3d7b2d8384c8d94a742bfc609232c896e5a0
James Baxter <james.baxter@focusrite.com>
2021-08-10 06:01:56 +0800

    Initial Commit

---
+++ Shared/CatTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatTrainApp.swift
+//  Shared
+//
+//  Created by James Baxter on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CatTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
